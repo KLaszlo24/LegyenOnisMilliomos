54688dad59df0621808c3e196a0a623fe9d5fe17
KLaszlo24 <krajczlaci234@gmail.com>
2025-05-16 09:45:36 +0200

    Sorkerdesek beolvasása készen van

--- Milliomos/JatekIndito.cs
+++ Milliomos/JatekIndito.cs
@@ -8,5 +8,20 @@ namespace Milliomos
 {
     internal class JatekIndito
     {
+        private Kerdesek kerdesek = new Kerdesek();
+        private List<string> segitsegek = new() { "kozonseg", "telefon", "felezo" };
+        private int AktualSzint = 0;
+        private int[] nyeremeny = { 10000, 20000, 50000, 100000, 250000, 500000, 750000, 1000000, 1500000, 2000000, 5000000, 10000000, 15000000, 25000000, 50000000 };
+
+        public void Indit()
+        {
+            string[] sorkerdesSorok = File.ReadAllLines("sorkerdes.txt");
+            List<Sorkerdes> sorkerdesek = new List<Sorkerdes>();
+            foreach (string sor in sorkerdesSorok)
+            {
+                sorkerdesek.Add(Sorkerdes.Szetszed(sor));
+            }
+            //Sorkerdesek beolvasasa commit
+        }
     }
 }
